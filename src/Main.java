19b295554ef44f4045b34bc8444f8f9c6380bd27
paulo <henriquepaulo0606@gmail.com>
2023-03-22 09:55:00 -0300

    Testando variavel com valor atribuido

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Declarado variaveis
+        int idade;
+
+        // Atribuindo valor para variavel
+        idade = 37;
+
+        // Testando variavel com valor atribuido
+        System.out.println(idade);
+
+        // Atribuido um novo valor para variavel
+        idade = 30 + 10;
+        // Testando variavel com novo valor atribuido
+        System.out.println(idade);
+
+        // Atribuido um novo valor para variavel
+        idade = (7 * 5) + 2;
+        // Testando variavel com novo valor atribuido
+        System.out.println(idade);
+    }
+}
